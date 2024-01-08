fb6e56ac601909a332460862ec359edf5a6332f2
Tsizehena Sarobidi <tsizehena223@gmail.com>
2024-01-08 10:04:37 +0300

    update : text-color

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import AboutIllustration from "../assets/code.svg";
 
 const About = ({ isDarkMode }) => {
     return (
-        <div id="about" className={`h-auto pt-32 pb-10 text-gray-600 ${isDarkMode && 'text-gray-300'}`}>
+        <div id="about" className={`h-auto pt-32 pb-10 ${isDarkMode ? 'text-gray-300' : 'text-gray-600'}`}>
             <div className="text-center font-bold mx-8">
                 <p className="text-base">
                     Welcome to my portfolio website!
@@ -12,7 +12,7 @@ const About = ({ isDarkMode }) => {
                 <p className="font-normal my-6 mx-0">Active, curious and insightful... I seek to improve myself and experiment in the field of Information Technology!</p>
                 <div className="mx-2">
                     <a href="#contact"><button className={`w-56 py-3 rounded-full shadow-md ${isDarkMode ? 'text-slate-700 my-bg' : 'my-bg-dark text-slate-300'}`}>Contact Me</button></a>
-                    <a href="#resume"><button className={`w-56 mt-4 border border-slate-500 my-bg py-3 rounded-full shadow-md ${isDarkMode ? 'text-slate-400 my-bg-dark' : 'text-slate-500'}`}>Download Resume</button></a>
+                    <a href="#resume"><button className={`w-56 mt-4 border border-slate-500 my-bg py-3 rounded-full shadow-md ${isDarkMode ? 'text-slate-300 my-bg-dark' : 'text-slate-500'}`}>Download Resume</button></a>
                 </div>
             </div>
             <div className="flex items-center justify-center mt-10">
