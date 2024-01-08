c372650db7e361b5ce620dddd6ce0395e19437e9
Tsizehena Sarobidi <tsizehena223@gmail.com>
2024-01-08 16:02:14 +0300

    update: words changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import AboutIllustration from "../assets/code.svg";
 
 const About = ({ isDarkMode }) => {
-    {/* Animation typing
-    --- npm install react-typed ---
-    var typed = new Typed(".changeWords", {
-        strings: ["", "Web Developper", "Etudiant en IT"],
-        typeSpeed: 100,
-        BackSpeed: 60,
-        loop: true
-    }) */}
+    const words = ["Sarobidi", "Web Developper", "IT Student"];
+    const [indexWord, setIndexWord] = useState(0);
+
+    useEffect(() => {
+        const index = setInterval(() => {
+            setIndexWord((nextWord) => (nextWord - 1 == words.length - 1 ? 0 : nextWord + 1));
+        }, 3000);
+        return clearInterval(index);
+    }, [])
     
     return (
         <div id="about" className={`h-auto pt-32 pb-10 ${isDarkMode ? 'text-gray-300' : 'text-gray-600'}`}>
@@ -17,7 +18,7 @@ const About = ({ isDarkMode }) => {
                 <p className="text-base">
                     Welcome to my portfolio website!
                 </p>
-                <h1 className="text-3xl mt-6">Hey there, I'm <span className="text-purple-700 text-2xl changeWords">Sarobidi</span> !</h1>
+                <h1 className="text-3xl mt-6">Hey there, I'm <span className="text-purple-700 text-2xl changeWords">{words[indexWord]}</span> !</h1>
                 <p className="font-normal my-6 mx-0">Active, curious and insightful... I seek to improve myself and experiment in the field of Information Technology!</p>
                 <div className="mx-2">
                     <a href="#contact"><button className={`w-56 py-3 rounded-full shadow-md ${isDarkMode ? 'text-slate-700 my-bg' : 'my-bg-dark text-slate-300'}`}>Contact Me</button></a>
