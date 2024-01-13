298acb4ac29c73adf9de2431e528cafb6b5db97a
Tsizehena <tsizehena223@gmail.com>
2024-01-13 20:34:09 +0300

    udpate: experience page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
+import Neovim from '../assets/projects/sonic-nvim.png';
+import Portfolio from '../assets/projects/portfolio.png';
 
-const Card = () => {
+const Card = ({ isDarkMode }) => {
+    const projects = {
+        1: {
+            "title": "Code",
+            "content": "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+            "image": Neovim,
+            "link": "https://github.com/tsizehena223/sonic-nvim"
+        },
+        2: {
+            "title": "Portfolio",
+            "content": "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+            "image": Portfolio,
+            "link": "https://tsizehena.onrender.com"
+        }
+    }
+
     return (
-        <div className="flex sm:block mt-4 mx-8 bg-white shadow-lg rounded-lg overflow-hidden">
-            <img
-                className="w-full h-32 object-cover"
-                src="https://placekitten.com/400/200"
-                alt="Card"
-            />
-            <div className="p-4">
-                <h2 className="text-xl font-semibold text-gray-800">Card Title</h2>
-                <p className="mt-2 text-gray-600">
-                    This is a sample card component with Tailwind CSS and React.js.
-                </p>
-            </div>
+        <div className='md:mx-12 lg:mx-20 six:flex'>
+            {Object.entries(projects).map(([key, project]) => (
+                <div key={key} className={`m-8 sh:mx-16 six:mx-6 ${isDarkMode ? 'bg-slate-800' : 'bg-light-t'} shadow-lg rounded-lg overflow-hidden`}>
+                    <a href={project.link} className='flex items-center justify-center' target='_blank'>
+                        <img
+                            className="w-auto h-48 md:h-64 mt-4"
+                            src={project.image}
+                            alt=""
+                        />
+                    </a>
+                    <span className={`mt-2 block w-full h-0.5 ${isDarkMode ? 'bg-slate-600' : 'bg-slate-400'}`}></span>
+                    <div className="p-4">
+                        <h2 className={`text-xl font-semibold ${isDarkMode ? 'text-slate-300' : 'text-gray-600'}`}>{project.title}</h2>
+                        <p className={`mt-2 text-gray-600 ${isDarkMode ? 'text-slate-400' : 'text-gray-600'}`}>{project.content}</p>
+                    </div>
+                </div >
+            ))}
         </div>
     );
 };
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,13 +2,12 @@ import Card from "../components/Card"
 
 const Project = ({ isDarkMode }) => {
     return (
-        <div id="projects" className={`-pt-10 my-16 lg:pt-20 h-auto md:flex ${isDarkMode ? 'my-bg-dark text-gray-300' : 'my-bg text-gray-600'}`} >
-            <h2 className="font-bold text-3xl ml-8">Projects</h2>
-            <div className="py-4 sm:flex">
-                <Card />
-                <Card />
+        <div id="projects" className={`my-6 lg:pt-20 h-auto ${isDarkMode ? 'my-bg-dark text-gray-300' : 'my-bg text-gray-600'}`} >
+            <h2 className={`font-bold text-3xl md:text-4xl lg:text-5xl text-center text-purple-600`}>Recent projects</h2>
+            <div className="py-6">
+                <Card isDarkMode={isDarkMode} />
             </div>
-        </div>
+        </div >
     )
 }
 
